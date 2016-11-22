1e1a0e8ba64678010a844db8b1f68bc0adb2752d
ejatkin <ejatkin@gmail.com>
2016-11-22 12:12:26 +0000

    Added cat image

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "hello!"
 end
@@ -10,4 +12,11 @@ end
 
 get '/joke' do
   "this is a joke!"
+end
+
+get '/cat' do
+  '<div class="brd"> <img src="http://bit.ly/1eze8aE" alt="Cat picture"> </div>
+  <style contenteditable>
+    .brd { border: 3px dashed red;}
+  </style>'
 end
